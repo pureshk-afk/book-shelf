f18e773f4d452a7724b85ee9063d8fe3cea13f73
astrhsptl <dctrback@bk.ru>
2024-02-27 15:05:31 +0300

    fix media

--- src/backend/server/urls.py
+++ src/backend/server/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -11,7 +12,7 @@ from .settings import DEBUG
 schema_view = get_schema_view(
     openapi.Info(
         title="API",
-        default_version='v1',
+        default_version="v1",
         description="API documentation",
     ),
     public=True,
@@ -19,23 +20,54 @@ schema_view = get_schema_view(
 )
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include([
-        path('docs/', include([
-            path('', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-schema"),
-            path('api.json/', schema_view.without_ui(cache_timeout=0), name='schema-swagger-ui'),
-            path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-        ])),
-    path('auth/', include('authsystem.urls')),
-    path('shelf/', include('book.urls')),
-    path('post/', include('post.urls')),
-    path('cart/', include('cart.urls')),
-    ]))
-] + static(
-    settings.STATIC_URL, document_root=settings.STATIC_ROOT
-) + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path(
+            "api/v1/",
+            include(
+                [
+                    path(
+                        "docs/",
+                        include(
+                            [
+                                path(
+                                    "",
+                                    schema_view.with_ui("swagger", cache_timeout=0),
+                                    name="swagger-schema",
+                                ),
+                                path(
+                                    "api.json/",
+                                    schema_view.without_ui(cache_timeout=0),
+                                    name="schema-swagger-ui",
+                                ),
+                                path(
+                                    "redoc/",
+                                    schema_view.with_ui("redoc", cache_timeout=0),
+                                    name="schema-redoc",
+                                ),
+                            ]
+                        ),
+                    ),
+                    path("auth/", include("authsystem.urls")),
+                    path("shelf/", include("book.urls")),
+                    path("post/", include("post.urls")),
+                    path("cart/", include("cart.urls")),
+                ]
+            ),
+        ),
+    ]
+    # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + [
+        re_path(
+            r"^media/(?P<path>.*)$",
+            serve,
+            {
+                "document_root": settings.MEDIA_ROOT,
+            },
+        ),
+    ]
 )
 
 
@@ -43,5 +75,5 @@ if DEBUG:
     import debug_toolbar
 
     urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
+        path("__debug__/", include(debug_toolbar.urls)),
     ]
