0a01b2bb9dede5e601f944b5c1b5fedb299b948a
astrhsptl <dctrback@bk.ru>
2024-02-27 17:18:41 +0300

    asd

--- src/backend/cart/serializers.py
+++ src/backend/cart/serializers.py
@@ -6,17 +6,17 @@ from .models import Cart, CartBook
 
 
 class CartBookSerializer(ModelSerializer):
-    book = BookSerializer(source='book_id', many=False, read_only=True)
+    book = BookSerializer(source="book_id", many=False, read_only=True)
 
     class Meta:
         model = CartBook
-        fields = ("__all__")
-        
+        fields = "__all__"
+
 
 class CartSerializer(ModelSerializer):
-    user = UserPresentationSerializer(source='user_id', many=False, read_only=True)
-    cart_books = CartBookSerializer(many=True, read_only=True)
+    user = UserPresentationSerializer(source="user_id", many=False, read_only=True)
+    cart_books_items = CartBookSerializer(many=True, read_only=True)
 
     class Meta:
         model = Cart
-        fields = ("__all__")
+        fields = "__all__"
--- src/backend/server/urls.py
+++ src/backend/server/urls.py
@@ -57,7 +57,6 @@ urlpatterns = (
             ),
         ),
     ]
-    # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + [
         re_path(
