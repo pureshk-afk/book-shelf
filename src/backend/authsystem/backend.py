cc964ff24c8cf49a356d7e4242806ec8dc9e0f24
astrhsptl <dctrback@bk.ru>
2024-02-27 14:33:21 +0300

    add user by token

--- src/backend/authsystem/backend.py
+++ src/backend/authsystem/backend.py
@@ -1,27 +1,24 @@
 import jwt
-
 from django.conf import settings
 from rest_framework import status
-from rest_framework.authentication import BaseAuthentication
-from rest_framework.authentication import (
-    get_authorization_header
-)
+from rest_framework.authentication import BaseAuthentication, get_authorization_header
 
 from authsystem.models import User
 
+
 class JWTAuthClass(BaseAuthentication):
-    def authenticate(self, request, username=None, passwword=None, token=None):
+    def authenticate(self, request, username=None, *args, **kwargs):
         headers = get_authorization_header(request)
-        email = request.data.get('email')
-        username = request.data.get('username')
+        email = request.data.get("email")
+        username = request.data.get("username")
 
         if headers:
             return self._auth_by_jwt(headers)
         elif email:
-            password = request.data.get('password')
-            return self._auth_by_email(email, password) 
+            password = request.data.get("password")
+            return self._auth_by_email(email, password)
         elif username:
-            password = request.data.get('password')
+            password = request.data.get("password")
             return self._auth_by_username(username, password)
 
         return None, False
@@ -35,10 +32,9 @@ class JWTAuthClass(BaseAuthentication):
 
         return data.errors, status.HTTP_400_BAD_REQUEST
 
-
     def _auth_by_jwt(self, headers):
-        auth_data = headers.decode('utf-8')
-        auth_token = auth_data.split(' ')
+        auth_data = headers.decode("utf-8")
+        auth_token = auth_data.split(" ")
 
         if len(auth_token) != 2:
             return None, False
@@ -46,8 +42,8 @@ class JWTAuthClass(BaseAuthentication):
         token = auth_token[1]
 
         try:
-            decoded = jwt.decode(token, settings.SECRET_KEY, 'HS256')
-            user = User.objects.get(pk=decoded['user_id'])
+            decoded = jwt.decode(token, settings.SECRET_KEY, "HS256")
+            user = User.objects.get(pk=decoded["user_id"])
             return user, True
         except:
             return None, False
@@ -57,18 +53,18 @@ class JWTAuthClass(BaseAuthentication):
             user = User.objects.get(email=email)
         except:
             return None, False
-        
+
         if user.check_password(password):
             return user, True
-        
+
         return None, False
-    
+
     def _auth_by_username(self, username, password):
         try:
             user = User.objects.get(username=username)
         except:
             return None, False
-        
+
         if user.check_password(password):
             return user, True
         return None, False
