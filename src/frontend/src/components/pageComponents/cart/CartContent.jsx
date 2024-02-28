6a7e753716fcb92b7a77aad61a369033628309a9
astrhsptl <dctrback@bk.ru>
2024-02-28 14:19:51 +0300

    asd

--- src/frontend/src/components/pageComponents/cart/CartContent.jsx
+++ src/frontend/src/components/pageComponents/cart/CartContent.jsx
@@ -1,16 +1,39 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import arrowBack from '../../../images/arrow.png';
+import { useAuthentication } from '../../../api/auth/AuthUtils';
+import { fetchCarts } from '../../../api/payload/LoadingData';
+import arrowBack from '../../../images/arrow.svg';
+import emptyCart from '../../../images/cart_no_items.png';
+import { CartTable } from './CartTable';
 
 export const CartContent = () => {
+  const [cart, setCart] = useState(null);
+
+  useEffect(() => {
+    const authenticate = useAuthentication();
+    authenticate.then(async (r) => {
+      const cartList = await fetchCarts({ user_id: r.id, closed: 'false' });
+      if (cartList.results.length > 0) setCart(cartList.results[0]);
+    });
+  }, []);
+
   return (
-    <div className='container'>
-      <article className='cart-head'>
-        <Link to={'/'}>
-          <img src={arrowBack} alt='' />
+    <div className='container-cart'>
+      <div className='cart-head'>
+        <Link to={'/'} className='back-link'>
+          <img src={arrowBack} alt='' className='arrow-icon' />
           Главная
         </Link>
         <h2 className='cart-title'>Корзина</h2>
-      </article>
+      </div>
+
+      <div className='cart-table'>
+        {cart?.cart_books_items?.length > 0 ? (
+          <CartTable cart={cart} />
+        ) : (
+          <img src={emptyCart} alt='' className='image__empty' />
+        )}
+      </div>
     </div>
   );
 };
---
+++ src/frontend/src/components/pageComponents/cart/CartTable.jsx
@@ -0,0 +1,8 @@
+export const CartTable = ({ cart }) => {
+  return (
+    <div className='table-cart__skeleton'>
+      <div className='table-head'></div>
+      <div className='table-content'></div>
+    </div>
+  );
+};
